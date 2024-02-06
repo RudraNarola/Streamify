164a7223b4f191281aab77e7ef986d7728768151
Bhaumil111 <bhaumilpanchal@gmail.com>
2024-02-07 00:09:55 +0530

    clear search button added

--- app/(user)/_components/navbar/Search.tsx
+++ app/(user)/_components/navbar/Search.tsx
@@ -4,19 +4,42 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SearchIcon } from "lucide-react";
 import React from "react";
+import { useState } from "react";
 
 const Search = () => {
-  function hanldeSubmit() {}
+  function handleSubmit() {}
+
+  const [search, setSearch] = React.useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  }
+
+  const handleClear = () => {
+    setSearch('');
+  }
+
+  console.log(search);
 
   return (
     <form
-      onSubmit={hanldeSubmit}
+      onSubmit={handleSubmit}
       className="relative w-full lg:w-[500px] flex items-center gap-1 px-4"
     >
-      <Input placeholder="Search" className="focus-visible:ring-1" />
+    
+
+      
+      <Input placeholder="Search" value={search} onChange={handleChange} className="focus-visible:ring-1"  />
+      {search && (
+        <Button onClick={handleClear} size="sm" variant="ghost">
+          X
+        </Button>
+      )}
       <Button type="submit" size="sm" variant="ghost">
         <SearchIcon className="h-5 w-5 text-muted-foreground" />
       </Button>
+
+
     </form>
   );
 };
