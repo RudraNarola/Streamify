ee65bd01962fe49aadfb2d15646a20f3f985cc11
Rudra Narola <114010315+RudraNarola@users.noreply.github.com>
2024-02-28 14:02:28 +0530

    added block functionality

--- app/(user)/_components/sidebar/Sidebar.tsx
+++ app/(user)/_components/sidebar/Sidebar.tsx
@@ -24,14 +24,11 @@ const Sidebar = async () => {
 
 export default Sidebar;
 
-
 export const SidebarSkeleton = () => {
   return (
-
     <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
       <RecommendedSkeleton />
       <FollowedChannelSkeleton />
-
     </aside>
   );
 };
