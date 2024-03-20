e83f54f102cad97104708ad3cbb2c2d35b00221f
Rudra Narola <114010315+RudraNarola@users.noreply.github.com>
2024-03-20 09:57:13 +0530

    next config fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    // remotePatterns: ["https://img.clerk.com/*", "https://utfs.io/*"],
-    domains: ["img.clerk.com", "utfs.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "**",
+      },
+    ],
   },
 };
+
+// images: {
+//   // remotePatterns: ["https://img.clerk.com/*", "https://utfs.io/*"],
+//   domains: ["img.clerk.com", "utfs.io"],
+// },
